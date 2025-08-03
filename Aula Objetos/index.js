6be20b8f2d4b8a8fea15a106c8e9c21b87a51f37
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-08-03 20:35:35 -0300

    exercicio de objetos

---
+++ Aula Objetos/index.js
@@ -0,0 +1,28 @@
+let cliente = {
+    nome:'Lucas',
+    idade:27,
+    temCartaoFidelidade: true,
+    favoritos: ['Bife Acebolado', 'Hamburguer']
+}
+
+console.log('o nome do cliente é ' +cliente.nome)
+console.log('sua idade é :'+ cliente.idade)
+console.log('Possui cartao fidelidade: '+cliente.temCartaoFidelidade)
+console.log('os seus pratos favoritos são: ' + cliente.favoritos)
+
+/*
+Desafiozinho pra você:
+Crie um objeto chamado cliente com os seguintes dados:
+
+nome
+
+idade
+
+temCartaoFidelidade (boolean: true/false)
+
+favoritos (array com 2 pratos favoritos)
+
+Depois, exiba essas informações no console de forma amigável! Se quiser, me manda aqui o código e eu te dou aquele feedback camarada. 😎
+
+E quando quiser avançar pro Dia 14, a gente começa a explorar listas de objetos (arrays com vários itens)! Tá ficando profissional, hein. 💼🖥️
+*/
