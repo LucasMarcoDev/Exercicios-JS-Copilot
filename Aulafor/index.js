159ccab831843dc753f6cd0debd4366f233f58df
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-08-12 16:37:35 -0300

    exercicio while JavaScript

---
+++ Aulafor/index.js
@@ -0,0 +1,7 @@
+//While loop enquanto, estrutura de loop indefinido, pois nao se sabe quantas vezes precisarei repetir o loop
+//Loop While sera executado apenas quando a condiçao for verdadeira
+let n= 0
+while(n < 10){
+    console.log(n)
+    n+=1
+}
