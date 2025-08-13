c9290f19ad727b8e0cd8d5d21af38e895febaaea
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-08-13 16:54:05 -0300

    Aula While e DO While

---
+++ Aula do while e while/index.js
@@ -0,0 +1,35 @@
+let i = 0
+
+do{
+    console.log('ola classe')
+    i++
+}while(i < 5)
+
+    
+    let n =0
+    while(n<10){
+        console.log('Lucas Marco')
+        n+=1
+    }
+
+
+/*
+While sera executado enquanto o teste logico usado for verdadeiro e do while tambem funcionara assim
+e sempre usaremos eles em situaçoes indefinidas ou seja sempre quando nao sabemos quantas vezes queremos
+usar o bloco de codigo em questao
+
+EM DO WHILE, primeiro executamos o codigo e depois realizamos o teste logico.
+Exemplo:let i = 0
+
+do{
+    console.log('ola classe')
+    i++
+}while(i < 5)
+
+EM WHILE, primeiro realizamos o teste logico e depois executamos o bloco de codigo:
+exemplo:
+let i = 0,
+while(i <=10){
+    console.log('ola classe')
+    i+=1
+*/
