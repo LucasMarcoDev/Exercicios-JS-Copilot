e12410a5ce35177db4ee44b2a118ce7cbc7c0943
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-08-19 16:03:52 -0300

    Aulas/exercicios de function

---
+++ Aula functioncfbcursos/functions.js
@@ -0,0 +1,10 @@
+function teste(){
+    let d1 = window.document.getElementById('d1');
+    let d2 = window.document.getElementById('d2');
+    let d3 = window.document.getElementById('d3');
+
+    d1.innerText = 'Sou um novo texto'
+    d2.innerText = 'Eu sou o texto 2'
+    d3.innerText = 'Sou eu denovo '
+}
+
