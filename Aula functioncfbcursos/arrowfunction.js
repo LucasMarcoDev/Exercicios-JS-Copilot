5f2b0a7b6585b49a99b696a11768da9eda6a9a8a
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-08-28 15:48:11 -0300

    Aula de arrow functions

---
+++ Aula functioncfbcursos/arrowfunction.js
@@ -0,0 +1,15 @@
+//arrow function
+
+const soma =(v1,v2)=>{return v1 +v2}
+console.log(soma(2,4))
+
+const nome=n=>{return n}
+console.log(nome('Lucas'))
+
+
+const add=n=>n+10
+console.log(add(10))
+
+
+
+
