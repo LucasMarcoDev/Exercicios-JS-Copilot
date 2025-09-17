65349647251de65aaabb519e3a2a83bc28c12351
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-09-17 11:53:56 -0300

    Aula QuerySelector

---
+++ Aula queryselector queryselectorall/index.js
@@ -0,0 +1,25 @@
+const divTodas=[...document.getElementsByTagName("div")]
+const cursosTodos =[...document.getElementsByClassName("curso")]
+const cursosC1 =[...document.getElementsByClassName("c1")]
+const cursosC2 =[...document.getElementsByClassName("c2")]
+const crusoEspecial =document.getElementById("c1")
+
+// const query_divTodas =[...document.querySelectorAll("div[class]")]
+const query_divTodas =[...document.querySelectorAll("div > p")]
+const query_cursosTodos =[...document.querySelectorAll(".curso")]
+const query_cursosC1 =[...document.querySelectorAll("c1,p")]
+const query_cursosC2 =[...document.querySelectorAll("c2")]
+const query_crusoEspecial =document.querySelectorAll("#c1")[0]
+
+console.log(query_crusoEspecial)
+console.log(query_cursosC2)
+
+// console.log(cursosTodos)
+// console.log(cursosC1)
+// console.log(cursosC2)
+// console.log(crusoEspecial)
+
+
+// cursosC1.map((el)=>{
+//     el.classList.add("destaque")
+// })
